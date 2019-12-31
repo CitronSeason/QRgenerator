78b4f913c0ab0d1ab69af7b284049c7b4d24d30c
Hiroki Uchida <kiroki.working@gmail.com>
2019-12-31 22:12:07 +0900

    Mode discrimination add

--- QRgenerator.py
+++ QRgenerator.py
@@ -1,10 +1,32 @@
+import re
 import sys
 import math
+
+class modeSpecifier:
+    NUMERAL = "0001"
+    ALPHANUMERIC = "0010"
+    OCTOBIT = "0100"
+
 
 class pycolor:
     BLACK = "\033[30m"
     WHITE = "\033[37m"
     END = "\033[1m"
+
+def generateQR(txt):
+    QRmode=""
+
+    #モード判別(Mode discrimination)
+    p1 = re.compile('[a-z0-9$%*+-./:\s]+')
+    p2 = re.compile('[\d]+')
+    if p1.fullmatch(txt) != None:
+        QRmode = modeSpecifier.ALPHANUMERIC
+    elif p2.fullmatch(txt) != None:
+        QRmode = modeSpecifier.NUMERAL
+    else:
+        QRmode = modeSpecifier.OCTOBIT
+
+    
 
 def retbin(bin):
     if bin == 1:
@@ -23,6 +45,6 @@ def printQR(str):
             sys.stdout.write('\n')
         i += 1
 
-        
+
 printQR("1001010101010101110101001")
 print('\n')
